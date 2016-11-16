39b78948965a9238a733c18aac698445f83f359a
Jonathan Buchner <websites@jonathanbuchner.com>
2016-11-16 14:48:32 -0800

    Add modle location and update dependency

---
+++ src/OurTravelBlog/Models/TravelBlogContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurTravelBlog.Models
+{
+    public class TravelBlogContext : DbContext
+    {
+        public virtual DbSet<Location> Locations { get; set;  }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TraveBlog;integrated security=True");
+        }
+    }
+}
